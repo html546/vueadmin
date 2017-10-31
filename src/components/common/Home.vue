d5f3347b22990719053ae363057732c18ef86c5d
html546 <15712859926@163.com>
2017-10-31 10:09:37 +0800

    jspangadmin

---
+++ src/components/common/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+      <v-head></v-head>
+      <v-slidebar></v-slidebar>
+      <div class="content">
+        <transition name="move" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </div>
+  </div>
+</template>
+
+<script>
+import vHead from './Header.vue'
+import vSlidebar from './Slidebar.vue'
+export default {
+  components:{
+      vHead,vSlidebar
+  }
+}
+</script>
---
+++ src/components/page/BasicCharts.vue
@@ -0,0 +1,64 @@
+<template>
+   <div>
+       <v-pageTitle vtitle='BasicCharts'></v-pageTitle>
+       <el-row :gutter="20">
+           <el-col :xs="24" :sm="24" :md="8" :lg="8">
+               <el-card class="box-card">
+                   <barChart></barChart>
+               </el-card>
+           </el-col>
+           <el-col :xs="24" :sm="24" :md="8" :lg="8">
+               <el-card class="box-card">
+                   <lineChart></lineChart>
+               </el-card>
+           </el-col>
+           <el-col :xs="24" :sm="24" :md="8" :lg="8">
+               <el-card class="box-card">
+                   <radarChart></radarChart>
+               </el-card>
+           </el-col>
+       </el-row>
+       <el-row :gutter="20">
+           <el-col :xs="24" :sm="24" :md="12" :lg="12">
+               <el-card class="box-card">
+                   <pieChart></pieChart>
+               </el-card>
+           </el-col>
+           <el-col :xs="24" :sm="24" :md="12" :lg="12">
+               <el-card class="box-card">
+                   <funnelChart></funnelChart>
+               </el-card>
+           </el-col>
+       </el-row>
+   </div>
+</template>
+<script>
+import vPageTitle from '../common/pageTitle.vue'
+import barChart from '../charts/barChart.vue'
+import lineChart from '../charts/lineChart.vue'
+import radarChart from '../charts/radarChart.vue'
+import pieChart from '../charts/pieChart.vue'
+import funnelChart from '../charts/fuunelChart.vue'
+export default {
+  components:{
+      vPageTitle,barChart,lineChart,radarChart,pieChart,funnelChart
+  },
+  methods:{
+
+  }
+}
+</script>
+<style scoped>
+    .el-col{
+        margin-bottom: 16px;
+    }
+    .material-icons{
+        font-size: 80px;
+        color: #ddd;
+    }
+    .cart-string{
+        height: 100px;
+        padding-top: 10px;
+        font-size: 1.1rem;
+    }
+</style>
